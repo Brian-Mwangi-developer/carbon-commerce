e598d72d40bdaf1b9e95bec2d59719cfb2f629da
PriscahC <opiyopriscah4@gmail.com>
2023-09-07 07:26:20 -0700

    edits

--- src/components/discount/Ddata.js
+++ src/components/discount/Ddata.js
@@ -1,18 +1,8 @@
 const Ddata = [
   {
-    cover: "./images/discount/discount-1.png",
-    name: "BenuX 2022",
+    cover: "https://s3-ap-southeast-1.amazonaws.com/hnsgsfp/9/images/energy-efficient-fridges.png",
+    name: "Fridge",
     price: "Ksh 25000",
-  },
-  {
-    cover: "./images/discount/discount-2.png",
-    name: "Sony TV 1080p",
-    price: "Ksh 40,500",
-  },
-  {
-    cover: "./images/discount/discount-3.png",
-    name: "Sony PS4",
-    price: "Ksh 10,000",
   },
   {
     cover: "./images/discount/discount-4.png",
@@ -20,9 +10,29 @@ const Ddata = [
     price: "Ksh 5100",
   },
   {
+    cover: "./images/discount/discount-8.png",
+    name: "Pune HD",
+    price: "Ksh 3000",
+  },
+  {
+    cover: "https://5.imimg.com/data5/VX/VH/IH/GLADMIN-10087457/solar-power-systems-500x500.jpg",
+    name: "Solar Panels",
+    price: "Ksh 40,500",
+  },
+  {
+    cover: "./images/discount/discount-9.png",
+    name: "Sony CCTV",
+    price: "Ksh 8000",
+  },
+  {
     cover: "./images/discount/discount-5.png",
     name: "Tony BGB",
     price: "Ksh 2000",
+  },
+  {
+    cover: " https://p.globalsources.com/IMAGES/PDT/B1188249611/High-Quality-Second-Hand-Laptops.jpg",
+    name: "Laptops",
+    price: "Ksh 10,000",
   },
   {
     cover: "./images/discount/discount-6.png",
@@ -34,15 +44,6 @@ const Ddata = [
     name: "Ranasonic 2022",
     price: "Ksh 30000",
   },
-  {
-    cover: "./images/discount/discount-8.png",
-    name: "Pune HD",
-    price: "Ksh 3000",
-  },
-  {
-    cover: "./images/discount/discount-9.png",
-    name: "Sony CCTV",
-    price: "Ksh 8000",
-  },
+  
 ]
 export default Ddata
