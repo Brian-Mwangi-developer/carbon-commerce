ea523dc30c80bf53867ab063d111418d1d526846
PriscahC <opiyopriscah4@gmail.com>
2023-09-07 09:03:20 -0700

    frontend

---
+++ src/components/map/map.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function map() {
+  return (
+    <div style="height: 100vh; width: 100vw;">
+        <iframe src="https://africageoportal.maps.arcgis.com/apps/instant/nearby/index.html?appid=75a0dd29c48443b88640c8903d90efe4" width="100%" height="100%" frameborder="0" style="border:0" allowfullscreen>iFrames are not supported on this page.</iframe>
+    </div>
+  )
+}
+
+export default map
